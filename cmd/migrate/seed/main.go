48d7057317d1ad70b1fd101ecc255d630ebb9edb
Zavista <84294353+Zavista@users.noreply.github.com>
2026-05-03 13:58:05 -0400

    Add database seeding function

---
+++ cmd/migrate/seed/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/zavista/social-api/internal/db"
+	"github.com/zavista/social-api/internal/env"
+	"github.com/zavista/social-api/internal/store"
+)
+
+func main() {
+	addr := env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable")
+	conn, err := db.New(addr, 3, 3, "15m")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	store := store.NewPostgresStorage(conn)
+
+	db.Seed(store, conn)
+}
